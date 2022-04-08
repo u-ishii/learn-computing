8b77cb80d6883193432a97f85a1b7792522ee7c4
u-ishii <yuki.ishii@ghelia.com>
2022-04-09 02:00:55 +0900

    csp

---
+++ src/csp.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Generic, List, Optional, TypeVar
+
+V = TypeVar("V")
+D = TypeVar("D")
+
+
+class Constraint(Generic[V, D], ABC):
+    def __init__(self, variables: List[V]) -> None:
+        self.variables = variables
+
+    @abstractmethod
+    def satisfied(self, assignment: Dict[V, D]) -> bool:
+        ...
+
+
+class CSP(Generic[V, D]):
+    def __init__(self, variables: List[V], domains: Dict[V, List[D]]) -> None:
+        self.variables = variables
+        self.domains = domains
+        self.constraints: Dict[V, List[Constraint[V, D]]] = {}
+        for variable in self.variables:
+            self.constraints[variable] = []
+            if variable not in self.domains:
+                raise LookupError()
+
+    def add_constraint(self, constraint: Constraint[V, D]) -> None:
+        for variable in constraint.variables:
+            if variable not in self.variables:
+                raise LookupError()
+            self.constraints[variable].append(constraint)
+
+    def consistent(self, variable: V, assignment: Dict[V, D]):
+        for constraint in self.constraints[variable]:
+            if not constraint.satisfied(assignment):
+                return False
+        return True
+
+    def backtracking_search(self, assignment: Dict[V, D] = {}) -> Optional[Dict[V, D]]:
+        if len(assignment) == len(self.variables):
+            return assignment
+        unassigned = [v for v in self.variables if v not in assignment]
+        first: V = unassigned[0]
+        for value in self.domains[first]:
+            local_assignment = assignment.copy()
+            local_assignment[first] = value
+            if self.consistent(first, local_assignment):
+                result = self.backtracking_search(local_assignment)
+                if result is not None:
+                    return result
+        return None
