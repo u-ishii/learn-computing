3b6289910aa017f284bc5354034cac5d1d1b9207
u-ishii <yuki.ishii@ghelia.com>
2022-04-09 03:48:00 +0900

    improve sudoku

--- src/sudoku.py
+++ src/sudoku.py
@@ -4,34 +4,31 @@ from src.csp import CSP, Constraint
 
 
 def main() -> None:
-    positions: List[Tuple[int, int]] = []
-    for x in range(9):
-        for y in range(9):
-            positions.append((x, y))
-    sudoku = {position: [1, 2, 3, 4, 5, 6, 7, 8, 9] for position in positions}
-    csp: CSP[Tuple[int, int], int] = CSP(positions, sudoku)
-    csp.add_constraint(SudokuConstraint(positions))
+    indices: List[int] = [i for i in range(81)]
+    sudoku = {index: [1, 2, 3, 4, 5, 6, 7, 8, 9] for index in indices}
+    csp: CSP[int, int] = CSP(indices, sudoku)
+    csp.add_constraint(SudokuConstraint(indices))
     solution = csp.backtracking_search()
     if solution is None:
         print("No solution found!")
     else:
+        for i in range(9):
+            print("".join([str(solution[i * 9 + j]) for j in range(9)]))
         print(solution)
 
 
-class SudokuConstraint(Constraint[Tuple[int, int], int]):
+class SudokuConstraint(Constraint[int, int]):
 
     numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-    def __init__(self, positions: List[Tuple[int, int]]) -> None:
-        super().__init__(positions)
-        self.positions = positions
+    def __init__(self, indices: List[int]) -> None:
+        super().__init__(indices)
+        self.indices = indices
 
-    def satisfied(self, assignment: Dict[Tuple[int, int], int]) -> bool:
-        if len(assignment) != 81:
+    def satisfied(self, assignment: Dict[int, int]) -> bool:
+        row = [assignment[j] for j in assignment.keys() if j < 9]
+        if len(row) != len(set(row)):
             return False
-        for i in range(9):
-            if set([assignment[(i, j)] for j in range(9)]) != self.numbers:
-                return False
         return True
 
 
