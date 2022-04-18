5dba6701e073a309f05fa0e95ec0ed860eedcac5
u-ishii <yuki.ishii@ghelia.com>
2022-04-18 22:04:06 +0900

    randomize

--- src/sudoku.py
+++ src/sudoku.py
@@ -1,3 +1,5 @@
+import random
+import time
 from typing import Dict, List
 
 from src.csp import CSP, Constraint
@@ -5,7 +7,7 @@ from src.csp import CSP, Constraint
 
 def main() -> None:
     indices: List[int] = [i for i in range(81)]
-    sudoku = {index: [1, 2, 3, 4, 5, 6, 7, 8, 9] for index in indices}
+    sudoku = {index: random.sample([1, 2, 3, 4, 5, 6, 7, 8, 9], 9) for index in indices}
     csp: CSP[int, int] = CSP(indices, sudoku)
     constraint = SudokuConstraint(indices)
     csp.add_constraint(constraint)
@@ -13,29 +15,28 @@ def main() -> None:
     if solution is None:
         print("No solution found!")
     else:
-        skip = 0
+        input("render?")
+        turn = 1
         for past in constraint.history:
-            if skip > 0:
-                skip -= 1
-            else:
-                line = input()
-                if len(line) > 0:
-                    skip = int(line)
-                _render_sudoku(past)
+            print(f"turn: {turn}\n{_convert_sudoku_text(past)}")
+            turn += 1
+            time.sleep(0.01)
 
 
-def _render_sudoku(sudoku: Dict[int, int]) -> None:
+def _convert_sudoku_text(sudoku: Dict[int, int]) -> str:
+    lines: List[str] = []
     for y in range(9):
         if y != 0 and y % 3 == 0:
-            print("─" * 6 + "┼" + "─" * 6 + "┼" + "─" * 6)
+            lines.append("─" * 6 + "┼" + "─" * 6 + "┼" + "─" * 6)
+        number_line = ""
         for x in range(9):
             if x != 0 and x % 3 == 0:
-                print("│", end="")
+                number_line += "│"
             i = y * 9 + x
             cell = chr(ord("０") + sudoku[i]) if i in sudoku else "  "
-            print(cell, end="")
-        print()
-    print()
+            number_line += cell
+        lines.append(number_line)
+    return "\n".join(lines)
 
 
 class SudokuConstraint(Constraint[int, int]):
