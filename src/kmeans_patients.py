dab0214d25edabf9939fa6218b765e387513555b
u-ishii <yuki.ishii@ghelia.com>
2022-05-09 11:31:36 +0900

    scatter

--- src/kmeans_patients.py
+++ src/kmeans_patients.py
@@ -1,20 +1,27 @@
 import csv
 from typing import List
 
+import matplotlib.pyplot as plt
+
 from src.kmeans import DataPoint, KMeans
 
-_AGE_HEADERS = ["10歳未満", "10代", "20代", "30代", "40代", "50代", "60代", "70代", "80代", "90代", "100歳以上"]
-
-_DATE_HEADERS: List[str] = [f"2022-04-{str(i).zfill(2)}" for i in range(1, 31)]
+_DATE_HEADER = "発症_年月日"
+_AGE_HEADER = "患者_年代"
+_DATE_OPTIONS: List[str] = [f"2022-04-{str(i).zfill(2)}" for i in range(1, 31)]
+_AGE_OPTIONS = ["10歳未満", "10代", "20代", "30代", "40代", "50代", "60代", "70代", "80代", "90代", "100歳以上"]
 
 
 if __name__ == "__main__":
     with open("data/patients.csv") as f:
         reader = csv.DictReader(f)
-        rows = [{"age": row["患者_年代"], "date": row["発症_年月日"]} for row in reader]
-    filtered_rows = [row for row in rows if row["age"] in _AGE_HEADERS and row["date"] in _DATE_HEADERS]
-    points = [DataPoint([_AGE_HEADERS.index(row["age"]), _DATE_HEADERS.index(row["date"])]) for row in filtered_rows]
-    k_means = KMeans(10, points)
-    clusters = k_means.run()
-    for index, cluster in enumerate(clusters):
-        print(f"Cluster {index}: {len(cluster.points)}")
+        points = [
+            DataPoint([_DATE_OPTIONS.index(row[_DATE_HEADER]), _AGE_OPTIONS.index(row[_AGE_HEADER])])
+            for row in reader
+            if row[_DATE_HEADER] in _DATE_OPTIONS and row[_AGE_HEADER] in _AGE_OPTIONS
+        ]
+    plt.scatter([p.dimensions[0] for p in points], [p.dimensions[1] for p in points])
+    plt.show()
+    # k_means = KMeans(10, points)
+    # clusters = k_means.run()
+    # for index, cluster in enumerate(clusters):
+    #     print(f"Cluster {index}: {len(cluster.points)}")
