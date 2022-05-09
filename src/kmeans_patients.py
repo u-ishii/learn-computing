c2757aaca70e5656d26dfbed7166103439bd9866
u-ishii <yuki.ishii@ghelia.com>
2022-05-09 21:21:02 +0900

    color kmeans

--- src/kmeans_patients.py
+++ src/kmeans_patients.py
@@ -1,11 +1,11 @@
 import csv
 from collections import defaultdict
 from dataclasses import dataclass
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Sequence, Tuple
 
 import matplotlib.pyplot as plt
 
-from src.kmeans import DataPoint, KMeans
+from src.kmeans import Cluster, DataPoint, KMeans
 
 _DATE_HEADER = "発症_年月日"
 _AGE_HEADER = "患者_年代"
@@ -24,9 +24,18 @@ class GroupedPatientRow(PatientRow):
     amount: int
 
 
+@dataclass
+class ScatterParams:
+    x: Sequence[int]
+    y: Sequence[int]
+    s: Sequence[int]
+    c: Sequence[int]
+
+
 class PatientPoint(DataPoint):
     def __init__(self, row: GroupedPatientRow):
         super().__init__([row.date, row.age, row.amount])
+        self.row: GroupedPatientRow = row
 
 
 def _read_patient_rows() -> List[PatientRow]:
@@ -46,17 +55,24 @@ def _group_patient_rows(rows: List[PatientRow]) -> List[GroupedPatientRow]:
     return [GroupedPatientRow(date, age, counted[date, age]) for date, age in counted.keys()]
 
 
-def _convert_as_scatter_params(rows: List[GroupedPatientRow]) -> Dict[str, Any]:
-    dates, ages, amounts = zip(*[(row.date, row.age, row.amount) for row in rows])
-    return {"x": dates, "y": ages, "s": amounts}
+def _convert_as_scatter_params(clusters: List[Cluster[PatientPoint]]) -> ScatterParams:
+    dates, ages, amounts, cluster_indices = zip(
+        *[
+            (point.row.date, point.row.age, point.row.amount, i)
+            for i, cluster in enumerate(clusters)
+            for point in cluster.points
+        ]
+    )
+    return ScatterParams(x=dates, y=ages, s=amounts, c=cluster_indices)
 
 
 if __name__ == "__main__":
     patient_rows = _read_patient_rows()
     grouped_patient_rows = _group_patient_rows(patient_rows)
-    plt.scatter(**_convert_as_scatter_params(grouped_patient_rows))
+    patient_points = [PatientPoint(row) for row in grouped_patient_rows]
+    k_means = KMeans(5, patient_points)
+    clusters = k_means.run()
+    plt.scatter(**_convert_as_scatter_params(clusters).__dict__)
     plt.show()
-    # k_means = KMeans(10, points)
-    # clusters = k_means.run()
     # for index, cluster in enumerate(clusters):
     #     print(f"Cluster {index}: {len(cluster.points)}")
