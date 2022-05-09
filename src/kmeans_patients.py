471961f1a7b0d562efa558dd3aa79e872f946f05
u-ishii <yuki.ishii@ghelia.com>
2022-05-09 20:50:40 +0900

    group

--- src/kmeans_patients.py
+++ src/kmeans_patients.py
@@ -1,5 +1,6 @@
 import csv
-from typing import List
+from collections import defaultdict
+from typing import Any, Dict, List, Tuple
 
 import matplotlib.pyplot as plt
 
@@ -10,16 +11,36 @@ _AGE_HEADER = "患者_年代"
 _DATE_OPTIONS: List[str] = [f"2022-04-{str(i).zfill(2)}" for i in range(1, 31)]
 _AGE_OPTIONS = ["10歳未満", "10代", "20代", "30代", "40代", "50代", "60代", "70代", "80代", "90代", "100歳以上"]
 
+PatientRow = Tuple[int, int]
+GroupedPatientRow = Tuple[int, int, int]
 
-if __name__ == "__main__":
+
+def _read_patient_rows() -> List[PatientRow]:
     with open("data/patients.csv") as f:
         reader = csv.DictReader(f)
-        points = [
-            DataPoint([_DATE_OPTIONS.index(row[_DATE_HEADER]), _AGE_OPTIONS.index(row[_AGE_HEADER])])
+        return [
+            (_DATE_OPTIONS.index(row[_DATE_HEADER]), _AGE_OPTIONS.index(row[_AGE_HEADER]))
             for row in reader
             if row[_DATE_HEADER] in _DATE_OPTIONS and row[_AGE_HEADER] in _AGE_OPTIONS
         ]
-    plt.scatter([p.dimensions[0] for p in points], [p.dimensions[1] for p in points])
+
+
+def _group_patient_rows(rows: List[PatientRow]) -> List[GroupedPatientRow]:
+    counted: Dict[Tuple[int, int], int] = defaultdict(lambda: 0)
+    for row in rows:
+        counted[row[0], row[1]] += 1
+    return [(date, age, counted[date, age]) for date, age in counted.keys()]
+
+
+def _convert_as_scatter_params(rows: List[GroupedPatientRow]) -> Dict[str, Any]:
+    dates, ages, amounts = zip(*rows)
+    return {"x": dates, "y": ages, "s": amounts}
+
+
+if __name__ == "__main__":
+    patient_rows = _read_patient_rows()
+    grouped_patient_rows = _group_patient_rows(patient_rows)
+    plt.scatter(**_convert_as_scatter_params(grouped_patient_rows))
     plt.show()
     # k_means = KMeans(10, points)
     # clusters = k_means.run()
