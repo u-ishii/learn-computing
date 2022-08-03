08f2b785ed25fc6c1bc9d6307de5a715e0f49d72
u-ishii <yuki.ishii@ghelia.com>
2022-08-04 02:10:03 +0900

    improve fitness

--- src/genetic_sudoku.py
+++ src/genetic_sudoku.py
@@ -10,8 +10,8 @@ def main() -> None:
     initial_population: List[SudokuChromosome] = [SudokuChromosome.random_instance() for _ in range(20)]
     algorithm: GeneticAlgorithm[SudokuChromosome] = GeneticAlgorithm(
         initial_population=initial_population,
-        threshold=9 * 3,
-        mutation_chance=0.5,
+        threshold=9 * 9 * 3,
+        mutation_chance=0.1,
         max_generations=10000,
     )
     result = algorithm.run()
@@ -45,46 +45,39 @@ def _get_constraint_ranges() -> List[List[int]]:
 
 class SudokuChromosome(Chromosome):
     def __init__(self, values: Sudoku) -> None:
+        assert len(values) == 81
         self.values: Sudoku = values
 
     def fitness(self) -> float:
-        assert self._validate_values()
         result = 0
         for constraint_range in _get_constraint_ranges():
             block = [self.values[i] for i in constraint_range]
-            if len(block) == len(set(block)):
-                result += 1
+            result += len(set(block))
         return result
 
     @classmethod
     def random_instance(cls) -> "SudokuChromosome":
-        random_values: Sudoku = {}
-        for i in range(9):
-            candidates = list(range(1, 10))
-            random.shuffle(candidates)
-            new_values = dict(zip(range(i * 9, (i + 1) * 9), candidates))
-            random_values = {**random_values, **new_values}
-        return SudokuChromosome(random_values)
+        candidates = [i for _ in range(9) for i in range(1, 10)]
+        random.shuffle(candidates)
+        values: Sudoku = dict(zip(range(81), candidates))
+        return SudokuChromosome(values)
 
     def crossover(self, other: "SudokuChromosome") -> Tuple["SudokuChromosome", "SudokuChromosome"]:
         child_values1: Sudoku = {**self.values}
         child_values2: Sudoku = {**other.values}
-        for row_range in _get_row_ranges():
+        for row_col_range in random.choice((_get_row_ranges(), _get_col_ranges())):
             if random.choice((True, False)):
-                for i in row_range:
+                for i in row_col_range:
                     child_values1[i], child_values2[i] = child_values2[i], child_values1[i]
+        # fmt: off
+        assert sum(child_values1.values()) + sum(child_values2.values()) == sum(self.values.values()) + sum(other.values.values())  # noqa: E501
         return SudokuChromosome(child_values1), SudokuChromosome(child_values2)
 
-    def mutate(self) -> None:
-        row_range = random.choice(_get_row_ranges())
-        i1, i2 = random.sample(row_range, k=2)
-        self.values[i1], self.values[i2] = self.values[i2], self.values[i1]
-
-    def _validate_values(self) -> bool:
-        for row_range in _get_row_ranges():
-            if len({self.values[i] for i in row_range}) != 9:
-                return False
-        return True
+    def mutate(self) -> "SudokuChromosome":
+        mutated_values: Sudoku = {**self.values}
+        i1, i2 = random.sample(range(81), k=2)
+        mutated_values[i1], mutated_values[i2] = mutated_values[i2], mutated_values[i1]
+        return SudokuChromosome(mutated_values)
 
     def __str__(self) -> str:
         return f"{convert_sudoku_text(self.values)}\nFitness: {self.fitness()}"
